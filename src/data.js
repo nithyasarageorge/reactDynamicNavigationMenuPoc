514221d214a12f02024fe70f092feb93092ce95b
nithyasarageorge <nithyasara.george@cognizant.com>
2018-03-08 11:43:12 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,28 @@
+var data = [
+  {
+    "no":0,
+    "name": "London",
+    "value": "London is the capital city of England. It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants."
+  },
+  {
+    "no": 1,
+    "name": "Paris",
+    "value": "Paris (French pronunciation: ​[paʁi] (About this sound listen)) is the capital and most populous city in France, with an administrative-limits area of 105 square kilometres (41 square miles) and an official population of 2,206,488 (2015).[5] The city is a commune and department, and the heart of the 12,012-square-kilometre (4,638-square-mile) Île-de-France region (colloquially known as the 'Paris Region'), whose 2016 population of 12,142,802 represented roughly 18 percent of the population of France"
+  },
+  {
+    "no": 2,
+    "name": "Tokyo",
+    "value": "Tokyo (/ˈtoʊkioʊ/, Japanese: [toːkʲoː] (About this sound listen)), officially Tokyo Metropolis,[6] is the capital city of Japan and one of its 47 prefectures.[7] The Greater Tokyo Area is the most populous metropolitan area in the world.[8] It is the seat of the Emperor of Japan and the Japanese government."
+  },
+  {
+    "no": 3,
+    "name": "England",
+    "value": "England, birthplace of Shakespeare and The Beatles, is a country in the British Isles bordering Scotland and Wales. The capital, London, on the River Thames, is home of Parliament, Big Ben and the 11th-century Tower of London. It's also a multicultural, modern hub for the arts and business. Other large cities are Manchester, Birmingham, Liverpool, Bristol and the university centres of Oxford and Cambridge."
+  },
+  {
+    "no": 4,
+    "name":"Clear All",
+    "value":""
+  }
+]
+export default data;
